f0445de29febf374bd3f3a76dfa08536fc8ba0e3
Student-2024 <student-2024@ashalim.org>
2025-03-27 11:10:07 +0200

    Added input validations to AdminUserEditActivity

--- app/src/main/java/com/roi/teammeet/adapters/UserEditAdapter.java
+++ app/src/main/java/com/roi/teammeet/adapters/UserEditAdapter.java
@@ -1,11 +1,14 @@
 package com.roi.teammeet.adapters;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.widget.SwitchCompat;
@@ -14,7 +17,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.roi.teammeet.R;
 import com.roi.teammeet.models.User;
 import com.roi.teammeet.services.DatabaseService;
+import com.roi.teammeet.utils.Validator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserEditAdapter extends RecyclerView.Adapter<UserEditAdapter.UserViewHolder> {
@@ -34,6 +39,8 @@ public class UserEditAdapter extends RecyclerView.Adapter<UserEditAdapter.UserVi
         notifyDataSetChanged();
     }
 
+
+
     @Override
     public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.user_card, parent, false);
@@ -46,7 +53,16 @@ public class UserEditAdapter extends RecyclerView.Adapter<UserEditAdapter.UserVi
         holder.tvUsername.setText(user.getUsername());
         holder.etUsername.setText(user.getUsername());
         holder.etBirthYear.setText(user.getBirthYear());
-        holder.etGender.setText(user.getGender());
+
+        ArrayList<String> genderArrayList = new ArrayList<>();
+        genderArrayList.add("male");
+        genderArrayList.add("female");
+        genderArrayList.add("other");
+
+        ArrayAdapter<String> genderSpinnerAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, genderArrayList);
+        genderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        holder.spinnerGender.setAdapter(genderSpinnerAdapter);
+
         holder.etPhone.setText(user.getPhone());
         holder.etEmail.setText(user.getEmail());
         holder.etPassword.setText(user.getPassword());
@@ -55,15 +71,20 @@ public class UserEditAdapter extends RecyclerView.Adapter<UserEditAdapter.UserVi
         databaseService = DatabaseService.getInstance();
 
         holder.btnUpdate.setOnClickListener(v -> {
-            String userName = holder.etUsername.getText().toString();
+            String username = holder.etUsername.getText().toString();
             String birthYear = holder.etBirthYear.getText().toString();
-            String gender = holder.etGender.getText().toString();
+            String gender = holder.spinnerGender.getSelectedItem().toString();
             String phone = holder.etPhone.getText().toString();
             String email = holder.etEmail.getText().toString();
             String password = holder.etPassword.getText().toString();
             boolean isAdmin = holder.switchAdmin.isChecked();
 
-            User updatedUser = new User(user.getId(), userName, birthYear, gender, phone, email, password, isAdmin);
+            Log.d(TAG, "onClick: Validating input...");
+            if (!holder.checkInput(username, phone, email, password)) {
+                return;
+            }
+
+            User updatedUser = new User(user.getId(), username, birthYear, gender, phone, email, password, isAdmin);
 
             databaseService.createNewUser(updatedUser, new DatabaseService.DatabaseCallback<Object>() {
                 @Override
@@ -102,7 +123,7 @@ public class UserEditAdapter extends RecyclerView.Adapter<UserEditAdapter.UserVi
         TextView tvUsername;
         EditText etUsername;
         EditText etBirthYear;
-        EditText etGender;
+        Spinner spinnerGender;
         EditText etPhone;
         EditText etEmail;
         EditText etPassword;
@@ -112,16 +133,48 @@ public class UserEditAdapter extends RecyclerView.Adapter<UserEditAdapter.UserVi
 
         public UserViewHolder(View itemView) {
             super(itemView);
-            tvUsername = itemView.findViewById(R.id.tvUsername_userCard);
+            tvUsername = itemView.findViewById(R.id.tvUsernameTitle_userCard);
             etUsername = itemView.findViewById(R.id.etUsername_userCard);
             etBirthYear = itemView.findViewById(R.id.etBirthYear_userCard);
-            etGender = itemView.findViewById(R.id.etGender_userCard);
+            spinnerGender = itemView.findViewById(R.id.spinnerGender_userCard);
             etPhone = itemView.findViewById(R.id.etPhone_userCard);
             etEmail = itemView.findViewById(R.id.etEmail_userCard);
             etPassword = itemView.findViewById(R.id.etPassword_userCard);
             switchAdmin = itemView.findViewById(R.id.switchAdmin_userCard);
             btnUpdate = itemView.findViewById(R.id.btnUpdate_userCard);
             btnDelete = itemView.findViewById(R.id.btnDelete_userCard);
+        }
+
+        private boolean checkInput(String username, String phone, String email, String password) {
+            if (!Validator.isUsernameValid(username)) {
+                Log.e(TAG, "checkInput: Username must be at least 3 characters long");
+                etUsername.setError("Username must be at least 3 characters long");
+                etUsername.requestFocus();
+                return false;
+            }
+
+            if(!Validator.isPhoneValid(phone)){
+                Log.e(TAG, "checkInput: Phone number must be 10 digits long");
+                etPhone.setError("Phone number must be 10 digits long");
+                etPhone.requestFocus();
+                return false;
+            }
+
+            if (!Validator.isEmailValid(email)) {
+                Log.e(TAG, "checkInput: Invalid email address");
+                etEmail.setError("Invalid email address");
+                etEmail.requestFocus();
+                return false;
+            }
+
+            if (!Validator.isPasswordValid(password)) {
+                Log.e(TAG, "checkInput: Password must be at least 8 characters long");
+                etPassword.setError("Password must be at least 8 characters long");
+                etPassword.requestFocus();
+                return false;
+            }
+
+            return true;
         }
     }
 }
